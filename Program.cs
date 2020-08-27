8355db1fb8c9de62dffafff20ec24c7b3d396a61
Tasha Lane <designtmg77@gmail.com>
2020-08-27 12:05:28 -0500

    initial files

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace calculator
+{
+    public class Calculator
+    {
+
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+        // ...other methods (e.g. Add(), Subtract(), Multiply())...
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+    }
+}
